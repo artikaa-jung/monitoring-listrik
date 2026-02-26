41947508a924d8d97c1ceedd04f85ee4694c0ddd
artikaa-jung <st4rnn621@gmail.com>
2026-02-26 21:58:12 +0700

    auth.js

---
+++ auth.js
@@ -0,0 +1,27 @@
+// ====== GANTI CREDENTIAL DEMO ======
+const DEMO_USER = "admin";
+const DEMO_PASS = "1234";
+
+function login(){
+  const u = document.getElementById("user").value.trim();
+  const p = document.getElementById("pass").value.trim();
+
+  if (u === DEMO_USER && p === DEMO_PASS) {
+    localStorage.setItem("isLogin", "1");
+    location.href = "index.html";
+  } else {
+    const err = document.getElementById("err");
+    err.style.display = "block";
+  }
+}
+
+function guard(){
+  if (localStorage.getItem("isLogin") !== "1") {
+    location.href = "login.html";
+  }
+}
+
+function logout(){
+  localStorage.removeItem("isLogin");
+  location.href = "login.html";
+}
